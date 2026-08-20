b053765c2e436088ecea471088778b8527b49316
Jeremy Siu <jeremy.siu@gmail.com>
2026-08-19 21:00:21 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def valid_char (self, s, counter):
+        ascii = ord(s[counter])
+        return (ascii >= ord('0') and ascii <= ord('9')) or (ascii >= ord('a') and ascii <= ord('z'))
+        
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        left_counter = 0;
+        right_counter = len(s) - 1
+        while left_counter < right_counter:
+            while self.valid_char(s, left_counter) == False:
+                if left_counter < len(s) - 1:
+                    left_counter += 1
+                else: 
+                    return True
+            while self.valid_char(s, right_counter) == False:
+                if right_counter > 0:
+                    right_counter -= 1
+                else:
+                    return True
+            if ord(s[left_counter]) != ord(s[right_counter]):
+                return False
+            left_counter += 1
+            right_counter -= 1
+        return True
+            
