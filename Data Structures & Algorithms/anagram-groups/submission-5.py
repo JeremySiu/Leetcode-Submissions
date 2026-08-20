9ee88595a511bbd011c7babb83d9feed57e6fb78
Jeremy Siu <jeremy.siu@gmail.com>
2026-08-20 17:00:14 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sublist_ascii = defaultdict(list)
+        anagrams = []
+        idx_counter = 0
+        for i in strs:
+            ascii_key = self.createKey(i)
+            if ascii_key in sublist_ascii:
+                anagrams[sublist_ascii[ascii_key]].append(i)
+            else:
+                sublist_ascii[ascii_key] = idx_counter
+                anagrams.append([])
+                anagrams[idx_counter].append(i)
+                idx_counter += 1
+        return anagrams
+
+
+    def createKey(self, string):
+        key = [0 for _ in range(26)]
+        for i in string:
+            key[ord(i)-ord('a')] += 1
+        return tuple(key)
